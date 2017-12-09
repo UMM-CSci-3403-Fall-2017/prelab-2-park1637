3b3e66a4320cef8438d8b74ebe7acae3fda0fb16
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-12-08 21:32:56 -0600

    change something i dont kno

--- check_whitespace.c
+++ check_whitespace.c
@@ -45,7 +45,7 @@ char* strip(char* str) {
   }
   // Place the null terminator at the end of the result string.
   result[i-first_non_space] = '\0';
-  free(result);
+// free(result);
   return result;
 }
 
@@ -66,12 +66,15 @@ int is_clean(char* str) {
   // 0 if they're equal, and a positive value if the first is
   // greater than the second.
   result = strcmp(str, cleaned);
+  free(cleaned);
+
   
-  if (cleaned[0] !='\0'){
-    
-    free(cleaned)
-      
+ /* if (cleaned[0] !='\0')
+  {
+	  
+	  free(cleaned);  
   }
+  */
   return result == 0;
 }
 
